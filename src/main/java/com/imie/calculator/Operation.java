18b6aa96966fe57d0f2c6b06eae46cb9bea344f3
Timothee Aupetit <timothee.aupetit@compte-nickel.fr>
2018-01-25 12:32:08 +0100

    made Operation a utility class

--- src/main/java/com/imie/calculator/Operation.java
+++ src/main/java/com/imie/calculator/Operation.java
@@ -1,18 +1,16 @@
 package com.imie.calculator;
 
 /**
- * This class defines the 4 usual operations
+ * This class defines the 4 usual operations.
  */
-public class Operation {
-
-    public Operation() {}
+public abstract class Operation {
 
     /**
      * @param a: first integer operand
      * @param b: second integer operand
      * @return a plus b
      */
-    public int add(int a, int b) {
+    public static int add(int a, int b) {
         return a + b;
     }
 
@@ -21,7 +19,7 @@ public class Operation {
      * @param b: second integer operand
      * @return a minus b
      */
-    public int subtract(int a, int b) {
+    public static int subtract(int a, int b) {
         return add(a, -b);
     }
 
@@ -30,7 +28,7 @@ public class Operation {
      * @param b: second float operand
      * @return a multiplied by b
      */
-    public float multiply(float a, float b) {
+    public static float multiply(float a, float b) {
         return a * b;
     }
 
@@ -39,7 +37,7 @@ public class Operation {
      * @param b: second float operand
      * @return a divided by b
      */
-    public float divide(float a, float b) {
+    public static float divide(float a, float b) {
         return multiply(a, 1 / b);
     }
 }
--- src/test/java/com/imie/calculator/OperationTest.java
+++ src/test/java/com/imie/calculator/OperationTest.java
@@ -4,48 +4,40 @@ import static org.junit.Assert.*;
 
 public class OperationTest {
 
-    private Operation operation;
-
     @org.junit.Before
     public void setUp() throws Exception {
-        this.operation = new Operation();
     }
 
     @org.junit.After
     public void tearDown() throws Exception {
-        this.operation = null;
     }
 
     @org.junit.Test
     public void add() {
-        assertNotNull(this.operation);
-        assertEquals(3, this.operation.add(1, 2));
-        assertEquals(-1, this.operation.add(1, -2));
+        assertEquals(3, Operation.add(1, 2));
+        assertEquals(-1, Operation.add(1, -2));
     }
 
     @org.junit.Test
     public void subtract() {
-        assertNotNull(this.operation);
-        assertEquals(7, this.operation.subtract(10, 3));
-        assertEquals(-6, this.operation.subtract(2, 8));
+        assertEquals(7, Operation.subtract(10, 3));
+        assertEquals(-6, Operation.subtract(2, 8));
     }
 
     @org.junit.Test
     public void multiply() {
-        assertNotNull(this.operation);
-        assertEquals(0, this.operation.multiply(0, 3), 0);
-        assertEquals(16, this.operation.multiply(2, 8), 0);
-        assertEquals(-8, this.operation.multiply(-1, 8), 0);
-        assertEquals(8, this.operation.multiply(-1, -8), 0);
+        assertEquals(0, Operation.multiply(0, 3), 0);
+        assertEquals(16, Operation.multiply(2, 8), 0);
+        assertEquals(-8, Operation.multiply(-1, 8), 0);
+        assertEquals(8, Operation.multiply(-1, -8), 0);
     }
 
     @org.junit.Test
     public void divide() {
-        assertNotNull(this.operation);
-        assertEquals(0.0, this.operation.divide(0, 3), 0);
-        assertEquals(4.0, this.operation.divide(8, 2), 0);
-        assertEquals(-4.0, this.operation.divide(8, -2), 0);
-        assertEquals(4.0, this.operation.divide(-8, -2), 0);
-        assertEquals(0.5, this.operation.divide(4, 8), 0);
+        assertEquals(0.0, Operation.divide(0, 3), 0);
+        assertEquals(4.0, Operation.divide(8, 2), 0);
+        assertEquals(-4.0, Operation.divide(8, -2), 0);
+        assertEquals(4.0, Operation.divide(-8, -2), 0);
+        assertEquals(0.5, Operation.divide(4, 8), 0);
     }
 }
