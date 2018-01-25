9f8e37dbd40ec005c50d9bdf069be029897c64ed
Timothee Aupetit <timothee.aupetit@compte-nickel.fr>
2018-01-25 11:55:39 +0100

    added constructor + comments

--- src/main/java/com/imie/calculator/Application.java
+++ src/main/java/com/imie/calculator/Application.java
@@ -1,6 +1,10 @@
 package com.imie.calculator;
 
 public class Application {
+
+    /**
+     * @param args: program parameters
+     */
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
--- src/main/java/com/imie/calculator/Operation.java
+++ src/main/java/com/imie/calculator/Operation.java
@@ -1,19 +1,44 @@
 package com.imie.calculator;
 
+/**
+ * This class defines the 4 usual operations
+ */
 public class Operation {
 
+    public Operation() {}
+
+    /**
+     * @param a: first integer operand
+     * @param b: second integer operand
+     * @return a plus b
+     */
     public int add(int a, int b) {
         return a + b;
     }
 
+    /**
+     * @param a: first integer operand
+     * @param b: second integer operand
+     * @return a minus b
+     */
     public int subtract(int a, int b) {
         return add(a, -b);
     }
 
+    /**
+     * @param a: first float operand
+     * @param b: second float operand
+     * @return a multiplied by b
+     */
     public float multiply(float a, float b) {
         return a * b;
     }
 
+    /**
+     * @param a: first float operand
+     * @param b: second float operand
+     * @return a divided by b
+     */
     public float divide(float a, float b) {
         return multiply(a, 1 / b);
     }
